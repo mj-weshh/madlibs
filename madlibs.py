31cfc2fe7da664a7c34f4c4da3cfa788555aab90
JOHN WAWERU MUHURA <jowamumjwesh@gmail.com>
2024-02-01 22:03:33 +0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+verb = input("Verb: ")
+duration = input("Duration(e.g; five hours): ")
+verb1 = input("Verb: ")
+
+
+madlib = f"Coding is fun and that's why I {verb} it. It keeps me \
+occupied for {duration} every day.{verb1} is like magic!!"
+
+print(madlib)
